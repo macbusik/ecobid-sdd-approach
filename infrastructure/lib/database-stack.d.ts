cde38545181b1273538e86f1b20e6097ca2fbb51
macio <macio@homeserver.homelab>
2026-02-18 03:22:52 -0500

    amplify init

--- infrastructure/lib/database-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
-export declare class DatabaseStack extends cdk.Stack {
-    readonly itemsTable: dynamodb.Table;
-    readonly seekersTable: dynamodb.Table;
-    readonly reservationsTable: dynamodb.Table;
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
